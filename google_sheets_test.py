865d2da9e63f95fe59f0c61a9e2523d9a37a8c36
stdragon233 <wanry2216@gmail.com>
2026-04-03 17:42:52 +0800

    Update google_sheets_test.py

--- google_sheets_test.py
+++ google_sheets_test.py
@@ -1,12 +1,16 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import os
+import json
 
 # Auth
 scope = ["https://www.googleapis.com/auth/spreadsheets"]
-creds = Credentials.from_service_account_file("credentials.json", scopes=scope)
+creds_dict = json.loads(os.environ["GOOGLE_CREDENTIALS"])
+creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
 client = gspread.authorize(creds)
 
-# Open sheet
-sheet = client.open("Test Automation").sheet1
+# Open sheet (use your sheet ID)
+SHEET_ID = "1mc4JNOVy9dPZEu2Xc-c9duRP6_otwNg2hbnGeZw9wV4"
+sheet = client.open_by_key(SHEET_ID).sheet1
 
 print("✅ Connected to Google Sheets!")
